ed7ac6591b35b82f38e1891d41b60eb7c9f7fe65
Jemima Farley-Rose <Mima@Laptop-for-Beans.local>
2020-11-11 14:17:24 +0000

    basic functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState} from 'react';
+import ReactPlayer from 'react-player'
 import './App.css';
 
-function App() {
+interface Image{
+    numberOfTimesVisible:number,
+    visible:boolean,
+    limit:number
+}
+
+interface Progress{
+  played: number;
+    playedSeconds: number;
+    loaded: number;
+    loadedSeconds: number;
+}
+
+const App=() =>{
+
+  
+
+  const [imageOneVisible, setImageOneVisible]=useState<Image>({visible:false, numberOfTimesVisible:0, limit:1} )
+  const [imageTwoVisible, setImageTwoVisible]=useState<Image>({visible:false, numberOfTimesVisible:0, limit:2})
+  const [imageThreeVisible, setImageThreeVisible]=useState<Image>({visible:false, numberOfTimesVisible:0, limit:3})
+  const [secondsPlayed, setSecondsPlayed]=useState<number>(0)
+
+  useEffect(()=>{
+    if(secondsPlayed > 3.5 && secondsPlayed < 8.5){
+      if( imageOneVisible.numberOfTimesVisible < imageOneVisible.limit && !imageOneVisible.visible ){
+        console.log('here')
+      setImageOneVisible(imageOneVisible=>({
+        ...imageOneVisible,
+        visible:true,
+        numberOfTimesVisible: imageOneVisible.numberOfTimesVisible ++
+      }))
+    }
+  }
+  if((secondsPlayed < 3.5 || secondsPlayed >8.5) && imageOneVisible.visible ){
+
+    setImageOneVisible(imageOneVisible=>({
+      ...imageOneVisible,
+      visible:false,
+    }))
+  
+}
+    if(secondsPlayed > 6.0 && secondsPlayed < 8.0){
+      if( imageTwoVisible.numberOfTimesVisible < imageTwoVisible.limit && !imageTwoVisible.visible){
+      setImageTwoVisible(imageTwoVisible=>({
+        ...imageTwoVisible,
+        visible:true,
+        numberOfTimesVisible: imageTwoVisible.numberOfTimesVisible ++
+      }))
+    }
+  }
+  if((secondsPlayed < 6.0 || secondsPlayed > 8.0) && imageTwoVisible){
+    setImageTwoVisible(imageTwoVisible=>({
+      ...imageTwoVisible,
+      visible:false,
+    }))
+  
+}
+    if(secondsPlayed > 7.0 && secondsPlayed < 8.5){
+      if( imageThreeVisible.numberOfTimesVisible < imageThreeVisible.limit && !imageThreeVisible.visible){
+      setImageThreeVisible(imageThreeVisible=>({
+        ...imageThreeVisible,
+        visible:true,
+        numberOfTimesVisible: imageThreeVisible.numberOfTimesVisible ++
+      }))
+    }
+  }
+  if((secondsPlayed < 7.0 || secondsPlayed > 8.5) && imageThreeVisible){
+    setImageThreeVisible(imageThreeVisible=>({
+      ...imageThreeVisible,
+      visible:false,
+    }))
+  
+}
+
+  },[secondsPlayed])
+
+  const onProgress = (state:Progress) => {
+    setSecondsPlayed(state.playedSeconds)
+  }
+
+  const renderImage=(imageState:Image, img:JSX.Element)=>{
+    if(imageState.visible && imageState.numberOfTimesVisible > imageState.limit){
+      return true
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="player">
+        <div className={"image-container"}>
+        <img className={
+         `image-one ${imageOneVisible.visible && 'visible'}`
+         } src={process.env.PUBLIC_URL + "/images/image1.png"}/>
+        
+        <img className={`image-two ${imageTwoVisible.visible && 'visible'}`}  
+         src={process.env.PUBLIC_URL + "/images/image2.png"}/>
+        
+        <img className={
+         `image-three ${imageThreeVisible.visible && 'visible'}`
+         } 
+          src={process.env.PUBLIC_URL + "/images/image3.png"}/>
+        </div>
+      <ReactPlayer 
+      url='video.mp4' 
+      controls={true}
+      muted={true} 
+      width='100%'
+      height='70%'
+      playing={true}
+      onStart={()=>{console.log('start')}}
+      onPause={()=>console.log('nay')}
+      onProgress={onProgress}
+      progressInterval={100}
+      />
+    </div>
     </div>
   );
 }
